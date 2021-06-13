7e861ff613b1f51e291cad2f813c23187ff48062
Veto Bastiero <vetobastiero@gmail.com>
2021-06-13 14:46:02 +0800

    feat: allows read file and print to console

--- frontend/public/electron.js
+++ frontend/public/electron.js
@@ -1,6 +1,5 @@
-const electron = require('electron'),
-  app = electron.app,
-  BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow, dialog, Menu } = require('electron');
+const fs = require('fs');
 
 const path = require('path'),
   isDev = require('electron-is-dev');
@@ -13,22 +12,116 @@ const createWindow = () => {
     ? 'http://localhost:3000'
     : `file://${path.join(__dirname, '../build/index.html')}`;
   mainWindow.loadURL(appUrl);
+
+  const isMac = process.platform === 'darwin';
+
+  const template = [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Open File',
+          accelerator: 'CmdOrCtrl+O',
+          click() {
+            openFile();
+          },
+        },
+      ],
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        ...(isMac
+          ? [
+              { role: 'pasteAndMatchStyle' },
+              { role: 'delete' },
+              { role: 'selectAll' },
+              { type: 'separator' },
+              {
+                label: 'Speech',
+                submenu: [{ role: 'startSpeaking' }, { role: 'stopSpeaking' }],
+              },
+            ]
+          : [{ role: 'delete' }, { type: 'separator' }, { role: 'selectAll' }]),
+      ],
+    },
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forceReload' },
+        { role: 'toggleDevTools' },
+        { type: 'separator' },
+        { role: 'resetZoom' },
+        { role: 'zoomIn' },
+        { role: 'zoomOut' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' },
+      ],
+    },
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'zoom' },
+        ...(isMac
+          ? [
+              { type: 'separator' },
+              { role: 'front' },
+              { type: 'separator' },
+              { role: 'window' },
+            ]
+          : [{ role: 'close' }]),
+      ],
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click: async () => {
+            const { shell } = require('electron');
+            await shell.openExternal('https://electronjs.org');
+          },
+        },
+      ],
+    },
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
   mainWindow.maximize();
   mainWindow.setFullScreen(false);
   mainWindow.on('closed', () => (mainWindow = null));
 };
+
 app.on('ready', createWindow);
 app.on('window-all-closed', () => {
-  // Follow OS convention on whether to quit app when
-  // all windows are closed.
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 app.on('activate', () => {
-  // If the app is still open, but no windows are open,
-  // create one when the app comes into focus.
   if (mainWindow === null) {
     createWindow();
   }
 });
+
+function openFile() {
+  const files = dialog.showOpenDialogSync(mainWindow, {
+    properties: ['openFile'],
+    filters: [{ name: 'Text', extensions: ['txt'] }],
+  });
+
+  if (!files) return;
+  const file = files[0];
+  const fileContent = fs.readFileSync(file).toString();
+  console.log(fileContent);
+}
